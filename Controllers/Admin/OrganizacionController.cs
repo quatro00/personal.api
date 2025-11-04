60ef0c6fe74050a6d5c271ec8efddde21e14085d
Carlos Garcia <josecarlosgarciadiaz@gmail.com>
2025-11-04 11:08:17 -0600

    upd

---
+++ Controllers/Admin/OrganizacionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace personal.api.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizacionController : ControllerBase
+    {
+    }
+}
